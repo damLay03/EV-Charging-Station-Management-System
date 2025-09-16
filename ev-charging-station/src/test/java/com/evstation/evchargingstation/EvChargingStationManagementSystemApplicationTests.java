f52f712fa110fb69de0aa96b6e5d426d9bbf9754
damLay03 <banhthucnhan@gmail.com>
2025-09-16 13:41:49 +0700

    First commit - Spring Boot backend

---
+++ ev-charging-station/src/test/java/com/evstation/evchargingstation/EvChargingStationManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evstation.evchargingstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvChargingStationManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
