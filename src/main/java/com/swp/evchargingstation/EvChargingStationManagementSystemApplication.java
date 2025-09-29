d3839414414b7b2806afd1d12691a9aeba0b2cbb
KhoaHTD <khoahuynh142005@gmail.com>
2025-09-29 15:52:49 +0700

    Init commit - renew project

---
+++ src/main/java/com/swp/evchargingstation/EvChargingStationManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.swp.evchargingstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvChargingStationManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvChargingStationManagementSystemApplication.class, args);
+	}
+
+}
